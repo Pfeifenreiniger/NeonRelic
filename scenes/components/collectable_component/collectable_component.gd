c48e64171a5ff67b843108bc3f0604a7fc9b096e
KVNSPTMN <kevin.spathmann@gmail.com>
2024-07-05 22:48:26 +0200

    added particles animation when heal up item is collected

--- scenes/components/collectable_component/collectable_component.gd
+++ scenes/components/collectable_component/collectable_component.gd
@@ -5,12 +5,20 @@ extends Area2D
 
 @export_enum("power_up", "heal_up") var collectable_type:String
 @export var collectable_resource_stats:Resource
+@export var collect_effect_particles_color:Color
+
+
+###----------NODE REFERENCES----------###
+
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D as GPUParticles2D
 
 
 ###----------METHODS: AT INITIATION CALLED----------###
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	gpu_particles_2d.global_position = (get_parent() as Sprite2D).global_position
+	gpu_particles_2d.modulate = collect_effect_particles_color
 
 
 ###----------CONNECTED SIGNALS----------###
@@ -26,4 +34,15 @@ func _on_body_entered(body:Node2D) -> void:
 		else:
 			printerr("No valid value of variable collectable_type")
 		
+		# disconnect custom signal to avoid another collect-action with this node
+		# while particle animation is playing
+		body_entered.disconnect(_on_body_entered)
+		# hide parent scene root node (is a Sprite2D) without hiding
+		# the child nodes (like the particles node)
+		(get_parent() as Sprite2D).texture = null
+		# start particles animation (is a one-shot) and wait
+		# for finish
+		gpu_particles_2d.emitting = true
+		await gpu_particles_2d.finished
+		# kill parent scene's root node
 		get_parent().queue_free()
